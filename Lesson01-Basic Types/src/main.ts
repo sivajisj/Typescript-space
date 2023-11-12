fcf9e5136cd3e86b258df53fc0892bc0d7f8dd00
gayatrisj sj <bhsgayathri2003@gmail.com>
2023-11-12 03:13:57 +0000

    lesson 1completed

---
+++ Lesson01-Basic Types/src/main.ts
@@ -0,0 +1,21 @@
+
+let myName: string = 'sivaji'         //string
+let myAge: number = 23               //number
+let isLoading: boolean;             //boolean
+let album: any;                    //any
+
+isLoading = false
+album = "sj23"
+
+
+const sum = (a: number,b: number) =>{
+    return a + b
+}
+
+ //union
+let postId: string | number
+let isActive: string | number
+
+//regex
+let re: RegExp = /\w+/g
+
