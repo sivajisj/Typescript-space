5d4fb58f1b3b606fef98180a9ab62e36b34503d0
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-11-13 07:14:03 +0000

    lesson 4 and 5 are added

---
+++ Lesson04-Assertions/src/copyright.ts
@@ -0,0 +1,24 @@
+
+// const year = document.getElementById('year') 
+// const thisYear = new Date().getFullYear() 
+
+// year?.setAttribute("datetime",thisYear) 
+// year?.textContent = thisYear 
+
+//1st variation
+// let year: HTMLElement | null
+// year = document.getElementById('year') 
+// let thisYear: string
+// thisYear = new Date().getFullYear().toString() 
+// if(year){
+//     year.setAttribute("datetime",thisYear) 
+//     year.textContent = thisYear 
+// }
+
+
+//2st variation
+const year = document.getElementById('year') as HTMLSpanElement
+const thisYear: string = new Date().getFullYear().toString()
+
+year.setAttribute("datetime", thisYear)
+year.textContent = thisYear 
